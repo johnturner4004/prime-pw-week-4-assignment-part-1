8d6e5024a5a5f36581795785769e6e84c6ba4b24
John Turner <johnturner4004@gmail.com>
2021-03-15 21:39:13 -0400

    Realized my other commits failed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -5,6 +5,10 @@ console.log('***** Function Practice *****')
 // to test it and display the result
 
 // 1. Function to return 'Hello World!'
+let question = 1;
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
+
 function hello() {
   return 'Hello World!';
 }
@@ -13,65 +17,207 @@ console.log('Test - should say "Hello World!"', hello());
 
 
 // 2. Function to return an personalized hello, for example 'Hello, Jo!'
-function helloName( name ) {
-  return;
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
+
+function helloName(name) {
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
+console.log(helloName('John'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
+
+function addNumbers(firstNumber, secondNumber) {
+  //Tests to if input is number to prevent errors
+  if (typeof firstNumber === 'number' && typeof secondNumber === 'number') {
+    // return firstNumber + secondNumber;
+    return firstNumber + secondNumber;
+  } else {
+    return 'Invalid input';
+  }
 }
 
+//Tests for possible input values
+console.log('Test #1 for addNumbers(1, 2):' + addNumbers(1, 2));
+console.log('Test #2 for addNumbers(1, -2):' + addNumbers(1, -2));
+console.log('Test #3 for addNumbers(-1, 2):' + addNumbers(-1, 2));
+console.log('Test #4 for addNumbers(-1, -2):' + addNumbers(-1, -2));
+console.log('Test #5 for addNumbers(Math.PI, 2):' + addNumbers(Math.PI, 2));
+console.log('Test #6 for addNumbers(\'one\', \'two\')' + addNumbers('one', 'two'));
+console.log('Test #7 for addNumbers(true, false)' + addNumbers(true, false));
+console.log('Test #8 for addNumbers(1, \'two\')' + addNumbers(1, 'two'));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
 
-}
-
-
-// 5. Function that will return true if a number is positive, 
-//    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
-    return;
+function multiplyThree(number) {
+  //Tests to only accept numbers as input to prevent errors
+  if (typeof number === 'number') {
+    number = number * 3;
+    return number;
+  } else {
+    return 'Invalid input';
   }
-    return;
 }
-// Call the function to test each outcome (true & false) 
+
+//varaible used to count incrementally number each test
+let test = 1;
+
+//tests various possible input values
+console.log(`Test #${test} for multiplyThree(2):`, multiplyThree(2));
+test++;
+console.log(`Test #${test} for multiplyThree(-2):`, multiplyThree(-2));
+test++;
+console.log(`Test #${test} for multiplyThree(0):`, multiplyThree(0));
+test++;
+console.log(`Test #${test} for multiplyThree(Math.PI):`, multiplyThree(Math.PI));
+test++;
+console.log(`Test #${test} for multiplyThree(\'two\'):`, multiplyThree('two'));
+test++;
+console.log(`Test #${test} for multiplyThree(true):`, multiplyThree(true));
+test++;
+
+// 5. Function that will return true if a number is positive,
+//    or greater than zero, and false otherwise
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
+
+function isPositive(number) {
+  //Tests to only accept numbers as input to prevent errors
+  if (typeof number === 'number') {
+    //Determins if number is positive
+    if (number > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return 'Invalid input';
+  }
+}
+
+//varaible used to count incrementally number each test
+test = 1;
+
+//tests various possible input values
+console.log(`Test #${test} for isPositive(2)`, isPositive(2));
+test++;
+console.log(`Test #${test} for isPositive(-2)`, isPositive(-2));
+test++;
+console.log(`Test #${test} for isPositive(0)`, isPositive(0));
+test++;
+console.log(`Test #${test} for isPositive(Math.PI)`, isPositive(Math.PI));
+test++;
+console.log(`Test #${test} for isPositive(\'two\')`, isPositive('two'));
+test++;
+console.log(`Test #${test} for isPositive(true)`, isPositive(true));
+test++;
+//oops I didn't realize there were already tests for multiple inputs.
+// Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log('isPositive - should say true', isPositive(3));
+console.log('isPositive - should say false', isPositive(0));
+console.log('isPositive - should say false', isPositive(-3));
 
 
-// 6. Function to return the _last_ item in an array. If the 
+// 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
-function getLast( array ) {
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
 
+function getLast(array) {
+  if (array != undefined && array.length > 0) {
+    return array[array.length - 1];
+  } else {
+    return undefined;
+  }
 }
 
-// 7. Function to find a value in an array. Return true if the 
+//declares variables and assigns values for various input types
+let numArray = [1, 2, 0, -5, 4];
+let strArray = ['This', 'will', 'be', 'used', 'to', 'test', 'getLast()'];
+let booArray = [true, true, false, true, false, false];
+let emptyArray;
+
+//varaible used to count incrementally number each test
+test = 1;
+
+//tests various possible input values
+console.log(`Test #${test} for getLast(numArray):`, getLast(numArray));
+test++;
+console.log(`Test #${test} for getLast(strArray):`, getLast(strArray));
+test++;
+console.log(`Test #${test} for getLast(booArray):`, getLast(booArray));
+test++;
+console.log(`Test #${test} for getLast(emptyArray):`, getLast(emptyArray));
+test++;
+console.log(`Test #${test} for getLast(arrayWithoutVariable):`, getLast([1, 2, 4, true, '#working!']));
+test++;
+
+// 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
-  
+//    DO NOT use Array.includes, Array.indexOf, or Array.find
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
+
+function find(value, array) {
+  if (value != undefined && array != undefined) {
+    //loop to search array for value
+    for (let i = 0; i < array.length; i++) {
+      if (value === array[i]) {
+        //retruns true if found
+        return true;
+      }
+    }
+
+    //returns false if not found in loop
+    return false;
+  } return false;
 }
+
+//varaible used to count incrementally number each test
+test = 1;
+
+let empty;
+
+//tests various possible input values
+console.log(`Test #${test} for find(-5, numArray):`, find(-5, numArray));
+test++;
+console.log(`Test #${test} for find(9, numArray):`, find(9, numArray));
+test++;
+console.log(`Test #${test} for find(\'test\', strArray):`, find('test', strArray));
+test++;
+console.log(`Test #${test} for find('\#fail\', numArray):`, find('#fail', strArray));
+test++;
+console.log(`Test #${test} for find(true, booArray):`, find(true, booArray));
+test++;
+console.log(`Test #${test} for find(-5, booArray):`, find(-5, booArray));
+test++;
+console.log(`Test #${test} for find(empty, booArray):`, find(empty, booArray));
+test++;
+console.log(`Test #${test} for find(-5, emptyArray):`, find(-5, emptyArray));
+test++;
 
 // ----------------------
 // Stretch Goals
 // ----------------------
-// 8. Function to check if a letter is the first letter in a 
+// 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
+console.log(`%cQuestion #${question}`, 'background:blue');
+question++;
+
 function isFirstLetter(letter, string) {
 
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
+console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll() {
   let sum = 0
   // TODO: loop to add items
   return sum;
@@ -83,6 +229,6 @@ function sumAll( ) {
 
 
 
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
+// 11. Pick a problem from Edabit(https://edabit.com/) or
+//     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
